7bf221477628563aae0e366b26e43fff6e1c7e97
batatich <timuraa008@gmail.com>
2026-05-18 22:49:29 +0300

    feat(frontend): add basic app router

--- frontend/src/pages/NotFoundPage.tsx
+++ frontend/src/pages/NotFoundPage.tsx
@@ -1,7 +1,13 @@
+import { Link } from 'react-router-dom'
+
 export function NotFoundPage() {
   return (
     <main>
       <h1>Страница не найдена</h1>
+
+      <p>Такого раздела в портале не существует.</p>
+
+      <Link to="/">Вернуться на главную</Link>
     </main>
-  );
+  )
 }
--- frontend/src/routes/AppRouter.tsx
+++ frontend/src/routes/AppRouter.tsx
@@ -1,6 +1,6 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { HomePage } from "../pages/HomePage";
-import { NotFoundPage } from "../pages/NotFoundPage";
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { HomePage } from '../pages/HomePage'
+import { NotFoundPage } from '../pages/NotFoundPage'
 
 export function AppRouter() {
   return (
@@ -10,5 +10,5 @@ export function AppRouter() {
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
-  );
+  )
 }
